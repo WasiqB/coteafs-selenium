671ccc2ef7eefdf544c9b02d7415da6f04450d6e
WasiqB <wasbhamla2005@gmail.com>
2019-05-05 22:01:55 +0530

    Updated to correct issue found on Beta1.

--- src/main/java/com/github/wasiqb/coteafs/selenium/config/AvailableBrowser.java
+++ src/main/java/com/github/wasiqb/coteafs/selenium/config/AvailableBrowser.java
@@ -1,22 +1,19 @@
 /**
- * Copyright (c) 2017-2020, Wasiq Bhamla.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2017-2020, Wasiq Bhamla. Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 package com.github.wasiqb.coteafs.selenium.config;
 
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
+
+import java.net.URL;
 
 import com.github.wasiqb.coteafs.selenium.constants.OS;
 
@@ -38,10 +35,10 @@ public enum AvailableBrowser {
 	 */
 	IE ("webdriver.ie.driver", "IEDriverServer");
 
-	private static final String	DEFAULT_FOLDER	= "/src/main/resources/drivers/";
+	private static final String DEFAULT_FOLDER = "drivers/";
 
-	private String				driver;
-	private String				key;
+	private String driver;
+	private String key;
 
 	private AvailableBrowser (final String key, final String driver) {
 		this.key = key;
@@ -52,10 +49,10 @@ public enum AvailableBrowser {
 	private void setup () {
 		if (getProperty (this.key) == null) {
 			final StringBuilder sb = new StringBuilder ();
-			String dir = getProperty ("user.dir");
-			sb.append (dir)
-				.append (DEFAULT_FOLDER)
-				.append (OS.platform ())
+			final URL dir = AvailableBrowser.class.getClassLoader ()
+				.getResource (DEFAULT_FOLDER);
+			sb.append (dir.getPath ());
+			sb.append (OS.platform ())
 				.append ("/")
 				.append (this.driver);
 			if (OS.isWindows ()) {
